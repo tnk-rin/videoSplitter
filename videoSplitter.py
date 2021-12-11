d269adc8ac87c64f53a31604c973d7076ec9b415
tnk-rin <tnk-rin@github.com>
2021-12-10 19:29:44 -0700

    Minor fixes

--- videoSplitter.py
+++ videoSplitter.py
@@ -6,10 +6,10 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-v', '--videopath', type=str, help='File path to the video clip', required=True)
 	parser.add_argument('-t', '--timecodepath', type=str, help='File path to the timecodes', required=True)
-	parser.add_argument('-y', '--year', type=str, help='Year to be added to the metadata', required=True)
-	parser.add_argument('-g', '--genre', type=str, help='Genre to be added to the metadata', required=True)
-	parser.add_argument('-a', '--album', type=str, help='Album name to be added to the metadata', required=True)
-	parser.add_argument('-f', '--force', help='Forces files to be replaced instead of skipping files that already exist', action='store_true')
+	parser.add_argument('-y', '--year', type=str, help='Year to be added to the metadata', default='')
+	parser.add_argument('-g', '--genre', type=str, help='Genre to be added to the metadata', default='')
+	parser.add_argument('-a', '--album', type=str, help='Album name to be added to the metadata', default='')
+	parser.add_argument('-f', '--force', help='Forces files to be replaced instead of skipping files that already exist', action='store_true', default=False)
 	args = parser.parse_args()
 	with open(args.timecodepath) as f:
 		times = f.readlines()
@@ -33,19 +33,18 @@ def main():
 		sclip.write_audiofile(path + '/' + filename[:len(filename)-1] + '.mp3', nbytes=2)
 		prevStartTime = timestamp
 		clipCount += 1
-		metaAdder.add_meta(path + '/' + filename[:len(filename)-1] + '.mp3', args.album, i + 1, len(times) + 1, args.year, args.genre)
-
+		metaAdder.add_meta(path + '/' + filename[:len(filename)-1] + '.mp3', args.album, i + 1, len(times), args.year, args.genre)
 	filename = times[len(times) - 1].split(' ')
 	filename.pop(0)
 	filename = (' '.join(filename))
-	if (os.path.exists(path + '/' + filename[:len(filename)-1] + '.mp3') and not args.force):
+	if (os.path.exists(path + '/' + filename[:len(filename)] + '.mp3') and not args.force):
 		print('"' + filename[:len(filename)-1] + '"\n\talready exists, skipping to next item\n')
 	else:
 		timestamp = clip.duration
 		sclip = clip.subclip(prevStartTime, timestamp).audio
-		sclip.write_audiofile(path + '/' + filename[:len(filename)-1] + '.mp3', nbytes=2)
+		sclip.write_audiofile(path + '/' + filename[:len(filename)] + '.mp3', nbytes=2)
 		del sclip
-		metaAdder.add_meta(path + '/' + filename[:len(filename)-1] + '.mp3', args.album, len(times) + 1, len(times) + 1, args.year, args.genre)
+		metaAdder.add_meta(path + '/' + filename[:len(filename)] + '.mp3', args.album, len(times), len(times), args.year, args.genre)
 	del clip
 	print("Completed")
 if(__name__ == "__main__"):
